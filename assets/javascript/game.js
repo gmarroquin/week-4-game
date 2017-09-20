229c580ab8e19ba005b046be3de584a38bfac086
Gary Marroquin <garymarroquin@Garys-MacBook-Pro.local>
2017-09-20 07:07:18 -0500

    start random number added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,11 @@ function initialize()
 {
 
 //Generating a random number between 19 and 120. "Var randomNumber" is creating 
+var beginGame = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+	console.log(beginGame);
+var audio = new Audio("Diamonds.mp3");
+
+
 var randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 	console.log(randomNumber);
 var audio = new Audio("Diamonds.mp3");
@@ -11,6 +16,10 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
 
+$("#startGame").click(function(){
+$("#startGame").replaceWith( beginGame );
+	console.log("StartGame?");
+
 //Onclick place random number in div
 $("#imageStyleOne, #imageStyleTwo, #imageStyleThree, #imageStyleFour").click(function(){
 $( "#randomNumberResult" ).replaceWith( randomNumber );
@@ -18,9 +27,12 @@ $( "#randomNumberResult" ).replaceWith( randomNumber );
 
 //play theme song
 audio.play();
+	console.log("Music added!!!!!")
 
 })
 
+
+})
 
 });
 
